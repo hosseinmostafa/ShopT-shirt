3f3317d81deee30fd4ab9b37bd8a8907d1d2e8c0
hosseinmostafa <hesseinnmostafaa@gmail.com>
2024-12-12 13:41:58 +0200

    update nav

--- src/app/Component/nav/nav.component.ts
+++ src/app/Component/nav/nav.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
-export class NavComponent {
+export class NavComponent implements OnInit{
+  showAlert: boolean = true;
+  message: string = 'Sign up and get 20% off to your first order 🎉';
+  button: string = 'Sign Up Now';
 
+  constructor() { }
+
+  private messages = [
+    { message: 'Sign up and get 20% off to your first order 🎉', button: 'Sign Up Now' },
+    { message: 'Limited time offer: Free shipping!', button: 'Shop Now' },
+    { message: 'Join our rewards program today!', button: 'Learn More' }
+  ];
+
+  private currentIndex: number = 0;
+
+
+
+  ngOnInit(): void {
+    this.startMessageRotation();
+  }
+
+  startMessageRotation() {
+    setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.messages.length;
+      this.message = this.messages[this.currentIndex].message;
+      this.button = this.messages[this.currentIndex].button;
+    }, 5000);
+  }
+
+  hideMessage() {
+    this.showAlert = false;
+  }
 }

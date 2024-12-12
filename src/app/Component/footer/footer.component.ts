c3c926b4705e0823c2e706009794f384dbc71554
hosseinmostafa <hesseinnmostafaa@gmail.com>
2024-12-12 22:35:03 +0200

    add footer

--- src/app/Component/footer/footer.component.ts
+++ src/app/Component/footer/footer.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FooterService } from '../../Service/footer.service';
+import { Subscription } from 'rxjs';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-footer',
@@ -7,4 +10,25 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+  showFooter: boolean = true;
+  subscription: Subscription;
+
+  constructor(private footerServes: FooterService, private spinner: NgxSpinnerService) {
+    this.subscription = this.footerServes.showFooter.subscribe((value) => {
+      this.showFooter = value;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
+    this.footerServes.displayFooter()
+  }
+
+  openSpinner1() {
+    this.spinner.show();
+
+    setTimeout(() => {
+      this.spinner.hide();
+    }, 2000);
+  }
 }
